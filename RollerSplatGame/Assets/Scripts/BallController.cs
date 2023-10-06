e0bc010ef9a996cf986ccd059778aaf0c3c2bfc0
theKorax <zikoramadin@gmail.com>
2023-10-06 20:27:29 +0100

    Updated BallController.cs
    Fixed bugs

--- RollerSplatGame/Assets/Scripts/BallController.cs
+++ RollerSplatGame/Assets/Scripts/BallController.cs
@@ -17,16 +17,7 @@ public class BallController : MonoBehaviour
     private Vector2 swipePosCurrentFrame;
     private Vector2 currentSwipe;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+   
 
     private void FixedUpdate()
     {
