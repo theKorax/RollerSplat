3bc83c10baaaf3d329801d2661b6316eee8a9bfc
theKorax <zikoramadin@gmail.com>
2023-10-06 19:20:59 +0100

    Updated BallController.cs Script
    Added a speed variable

--- RollerSplatGame/Assets/Scripts/BallController.cs
+++ RollerSplatGame/Assets/Scripts/BallController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+
+    public Rigidbody rb;
+    public float speed = 15.0f;
     // Start is called before the first frame update
     void Start()
     {
