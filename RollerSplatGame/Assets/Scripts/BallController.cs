6f1609fed4cf8549389370986cc318d13d27c522
theKorax <zikoramadin@gmail.com>
2023-10-06 21:10:25 +0100

    Updated BallController.cs Script
    Updated the script to change the colour of the ball at the start of the game and change the colour of the ground pieces as the ball rolls over them

--- RollerSplatGame/Assets/Scripts/BallController.cs
+++ RollerSplatGame/Assets/Scripts/BallController.cs
@@ -16,14 +16,32 @@ public class BallController : MonoBehaviour
     private Vector2 swipePosLastFrame;
     private Vector2 swipePosCurrentFrame;
     private Vector2 currentSwipe;
+
+    private Color solveColor;
     // Start is called before the first frame update
-   
+    private void Start()
+    {
+        solveColor = Random.ColorHSV(0.5f, 1);
+        GetComponent<MeshRenderer>().material.color = solveColor;
+    }
 
     private void FixedUpdate()
     {
         if (isTraveling)
         {
             rb.velocity = speed * travelDirection;
+        }
+
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position - (Vector3.up / 2), 0.05f);
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+            GroundPiece ground = hitColliders[i].transform.GetComponent<GroundPiece>();
+            if(ground && !ground.isColoured)
+            {
+                ground.ChangeColour(solveColor);
+            }
+            i++;
         }
 
         if(nextCollisionPosition != Vector3.zero)
