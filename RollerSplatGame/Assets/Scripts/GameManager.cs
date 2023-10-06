dc3648d85e1c4a7285a47fce8c902686a16f25e5
theKorax <zikoramadin@gmail.com>
2023-10-06 19:11:27 +0100

    Updated GameManager.cs Script
    Updated the script to find all objects of GroundPiece type as soon as the game starts

--- RollerSplatGame/Assets/Scripts/GameManager.cs
+++ RollerSplatGame/Assets/Scripts/GameManager.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static GameManager singleton;
+
+    private GroundPiece[] allGroundPieces;
     void Start()
     {
-        
+        SetUpNewLevel();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetUpNewLevel()
     {
-        
+        allGroundPieces = FindObjectsOfType<GroundPiece>();
     }
 }
